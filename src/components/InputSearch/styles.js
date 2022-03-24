c4636bcfc8a2746d9a14183832a53c1c68d7f38b
Emerson916 <emersons.a296@gmail.com>
2022-03-24 16:56:15 -0300

    Adicionando componente de pesquisa

---
+++ src/components/InputSearch/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const InputContainer = styled.View`
+  width: 85%;
+  height: 50px;
+  background-color: #f0f0f0;
+  display: flex;
+  margin: 20px auto 0px auto;
+  flex-direction: row;
+  border-radius: 8px;
+  padding-left: 15px;
+  align-items: center;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  font-size: 16px;
+  margin-left: 10px;
+  color: #8888;
+  font-weight: 500;
+`;
