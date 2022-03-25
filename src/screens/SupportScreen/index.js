98a9972786479ddaa635617dafb337bc35091639
Emerson916 <emersons.a296@gmail.com>
2022-03-25 15:12:49 -0300

    Estilizando e estruturando tela de configuração

--- src/screens/SupportScreen/index.js
+++ src/screens/SupportScreen/index.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { Container } from "./styles";
-import { StyleSheet } from "react-native";
+import {
+  Container,
+  ContainerDataUser,
+  TextName,
+  TextEmail,
+  ContainerConfig,
+} from "./styles";
+import { StyleSheet, ScrollView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import Title from "../../components/Title";
+import ButtonPremium from "../../components/ButtonPremium";
+import PremiumIcon from "../../assets/premiumIcon.svg";
+import { Avatar } from "react-native-paper";
+import ButtonConfig from "../../components/ButtonConfig";
+import ArrowRight from "../../assets/arrowRight.svg";
 
 const SupportScreen = () => {
   return (
@@ -11,7 +23,29 @@ const SupportScreen = () => {
         start={{ x: 1, y: 0 }}
         end={{ x: 1, y: 1.0 }}
         colors={["#343746", "#303C76"]}
-      ></LinearGradient>
+      >
+        <ScrollView>
+          <ContainerDataUser>
+            <Avatar.Image
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/77053593?v=4",
+              }}
+            />
+
+            <TextName>Emerson Silva</TextName>
+            <TextEmail>emersons.a296@gmail.com</TextEmail>
+            <ButtonPremium IconSvg={PremiumIcon} title="Seja Premium" />
+          </ContainerDataUser>
+
+          <ContainerConfig>
+            <Title text="CONFIGURAÇÕES" />
+            <ButtonConfig title="Editar Perfil" IconSvg={ArrowRight} />
+            <ButtonConfig title="Permição de dados" IconSvg={ArrowRight} />
+            <ButtonConfig title="Permição de Notificação" IconSvg={ArrowRight} />
+            <ButtonConfig title="Sair" IconSvg={ArrowRight} />
+          </ContainerConfig>
+        </ScrollView>
+      </LinearGradient>
     </Container>
   );
 };
