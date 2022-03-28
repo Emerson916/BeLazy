f030c6d82669687b8668ed6f8a462152856d7a21
Emerson916 <emersons.a296@gmail.com>
2022-03-28 16:46:26 -0300

    Colocando backgroundo na page

--- src/screens/FavoriteScreen/index.js
+++ src/screens/FavoriteScreen/index.js
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet } from "react-native";
+import { Container } from "./styles";
 
 const FavoritesScreen = () => {
   return (
-    <View>
-      <Text>FavoritesVideos</Text>
-    </View>
+    <Container>
+      <LinearGradient
+        style={styles.linearGradient}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 1, y: 1.0 }}
+        colors={["#343746", "#303C76"]}
+      ></LinearGradient>
+    </Container>
   );
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+});
