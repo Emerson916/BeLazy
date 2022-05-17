6d1a93ebeeb9341c390289c895c5041fa0fb8384
Emerson916 <emersons.a296@gmail.com>
2022-05-17 16:05:24 -0300

    Instalando novas depedências

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,17 @@
+const log = require("./middleware/logger");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
 
+const PORT = parseInt(process.env.PORT ?? process.env.port ?? "3000");
+
+const createUser = require("./endpoints/users/create-user");
+app.use(bodyParser.json());
+app.use(log());
+
+createUser(app);
+
+app.listen(PORT, () => {
+  console.log("Aplicação rodando na porta", PORT);
+});
