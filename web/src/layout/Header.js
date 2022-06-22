404a8936dcff028ecb925465c9cee244a5adc9a3
Emerson916 <emersons.a296@gmail.com>
2022-06-22 09:22:46 -0300

    feat: Criando o Header menu do site

---
+++ web/src/layout/Header.js
@@ -0,0 +1,12 @@
+import logo from "../assets/img/logo.svg"
+import iconMenu from "../assets/img/iconMenu.svg"
+
+const Header = () => {
+  return (
+    <div className="w-full h-24 bg-gray-900 flex justify-between items-center px-10">
+        <img src={logo} alt="Logo do site" className="h-20 w-20"/>
+        <img src={iconMenu} alt="Logo do site" className="h-10 w-10"/>
+    </div>
+  );
+};
+export default Header;
--- web/src/views/users/Home.js
+++ web/src/views/users/Home.js
@@ -1,9 +1,11 @@
 import CardsVideos from "../../components/CardsVideos";
+import Header from "../../layout/Header";
 
 const Home = () => {
   return (
-    <div className="h-screen px-10 bg-gradient-to-b from-current to-purple-800">
-        <CardsVideos />
+    <div className="h-screen bg-gradient-to-b from-current to-purple-800">
+      <Header />
+      <CardsVideos />
     </div>
   );
 };
