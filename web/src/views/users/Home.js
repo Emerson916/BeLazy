352bc78c51dbc08b72709e1ba5e7a7cfc8caaa09
Emerson916 <emersons.a296@gmail.com>
2022-06-22 10:21:36 -0300

    feat: Adicionando imageCarousel

--- web/src/views/users/Home.js
+++ web/src/views/users/Home.js
@@ -1,10 +1,32 @@
 import CardsVideos from "../../components/CardsVideos";
+import ImageCarousel from "../../components/ImageCarousel/ImageCarousel";
 import Header from "../../layout/Header";
 
 const Home = () => {
   return (
-    <div className="h-screen bg-gradient-to-b from-current to-purple-800">
+    <div className="h-full bg-gradient-to-b from-current to-purple-800">
       <Header />
+      <ImageCarousel />
+      <div className="flex">
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+      </div>
+
+      <div className="flex">
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+        <CardsVideos />
+      </div>
+
       <div className="flex">
         <CardsVideos />
         <CardsVideos />
